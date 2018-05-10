ecdfa55eab71def8e86515b445575661dc4b0955
Guive Jalili <guive.jalili@epitech.eu>
2018-05-09 22:06:48 -0700

    add cross origin

--- server.js
+++ server.js
@@ -28,6 +28,30 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
+// Add headers
+router.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Pass to next layer of middleware
+  next();
+});
+
+router.options("/*", function (req, res) {
+
+  // Request methods you wish to allow
+
+  // Request headers you wish to allow
+  //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  //res.setHeader('Access-Control-Allow-Credentials', true);
+  res.sendStatus(200);
+});
+
 app.use('/', require('./routes/user').bAuth)
 
 app.use(function (req, res, next) {
@@ -63,7 +87,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   console.error(err)
   res.status(err.code || 500).json({
-    success : false,
+    success: false,
     message: err.message || 'Something broke!'
   })
 })
